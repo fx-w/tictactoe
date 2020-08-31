3f927331b2f9f66dce9255a9ccf9aea69cf4cfd7
felix <felixweber777@gmail.com>
2020-08-31 19:15:47 +0200

    added try except to catch data type error

--- Move.py
+++ Move.py
@@ -1,35 +1,31 @@
-
 import Board
 
-def make_move(player):
 
+def make_move(player):
     success = False
 
     while not success:
 
         if player == 1:
 
-            place = int(input("Where would you like to place X ? \n1 - 9: "))
+            try:
+                place = int(input("Where would you like to place X ? \n1 - 9: "))
+                if 1 <= place <= 9 and Board.board[place - 1] == "E":
+                    Board.board[place - 1] = "X"
+                    success = True
 
-            if 1 <= place <= 9 and Board.board[place - 1] == "E":
-
-                Board.board[place -1] = "X"
-                success = True
-
-            else:
-                success = False
-                # please enter valid stuff (print)
-
-        else:
-
-            place = int(input("Where would you like to place O ? \n1 - 9: "))
-
-            if 1 <= place <= 9 and Board.board[place - 1] == "E":
-
-                Board.board[place - 1] = "O"
-                success = True
-
-            else:
+            except:
                 success = False
 
-# TODO: catch data type errors
+
+        else:  # player must be 2
+
+            try:
+
+                place = int(input("Where would you like to place O ? \n1 - 9: "))
+                if 1 <= place <= 9 and Board.board[place - 1] == "E":
+                    Board.board[place - 1] = "O"
+                    success = True
+
+            except:
+                success = False
